21fea4e6770f6084697676c7b16b3870df3897f2
Ananya Sahu <ananyasahu59513@gmail.com>
2025-04-10 20:11:30 +0530

    deep copy prep

--- Constructor.java
+++ Constructor.java
@@ -44,36 +44,78 @@
 
 //Shallow Copy of Resference type: copies address and not value;
 
+// class One{
+//     int arr[];
+//     One(int arr[]){
+//         this.arr=arr;
+//      }
+//      One(One obj){
+//         this.arr=obj.arr;
+//      }
+//      void display(){
+//         for(int i:arr){
+//             System.out.print(i + " ");
+//          }
+//          System.out.println();
+//      }
+// }
+
+// public class Constructor{
+//     public static void main(String args[]){
+//         int arr[]={1,2,3,4};
+
+//         One obj=new One(arr);
+//         One obj1=new One(obj);
+
+//         obj.display();
+//         obj1.display();
+
+//         obj.arr[0]=9;
+
+//         obj.display();
+//         obj1.display();
+
+//     }
+// }
+
+
+
+
+
+// Deep copy : Allocates new memory to the object.
+
 class One{
     int arr[];
     One(int arr[]){
         this.arr=arr;
-     }
-     One(One obj){
-        this.arr=obj.arr;
-     }
-     void display(){
+    }
+    One(One obj){
+        this.arr=new int[obj.arr.length];
+        for(int i=0;i<obj.arr.length;i++){
+            this.arr[i]=obj.arr[i];
+        }
+    }
+    void display(){
         for(int i:arr){
-            System.out.print(i + " ");
-         }
-         System.out.println();
-     }
+            System.out.print(i+" ");
+        }
+        System.out.println();
+    }
 }
 
 public class Constructor{
     public static void main(String args[]){
-        int arr[]={1,2,3,4};
-
+        int arr[]={3,6,9};
         One obj=new One(arr);
         One obj1=new One(obj);
-
+      
         obj.display();
         obj1.display();
 
-        obj.arr[0]=9;
+        obj.arr[0]=2;
 
         obj.display();
         obj1.display();
-
+    
     }
 }
