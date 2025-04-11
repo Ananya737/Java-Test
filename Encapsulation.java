f0bae38ca7a52dd1aa9e562a8426fbc038f5e74c
Ananya Sahu <ananyasahu59513@gmail.com>
2025-04-11 10:31:01 +0530

    Encapsulation

---
+++ Encapsulation.java
@@ -0,0 +1,23 @@
+
+class One{
+    private String name;
+
+    public void setName(String name){
+        this.name=name;
+    }
+    public void getName(){
+        System.out.println(name);
+    }
+
+}
+
+
+
+public class Encapsulation {
+    public static void main(String args[]){
+        One obj=new One();
+        obj.setName("Ananya");
+        obj.getName();
+    }
+    
+}
