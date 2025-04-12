8cae5fa8555fb24c4aad19b74e2015b07d100dda
Ananya Sahu <ananyasahu59513@gmail.com>
2025-04-12 13:02:58 +0530

    Ques 3

--- Ques.java
+++ Ques.java
@@ -47,33 +47,75 @@
 
 
 
-class Cons{
-    String name;
-    double roll;
-    int marks;
+// class Cons{
+//     String name;
+//     double roll;
+//     int marks;
 
-    Cons(String name){
-        this.name=name;
-    }
-    Cons(String name,double roll){
-        this.name=name;
-        this.roll=roll;
-    }
-    Cons(String name,double roll,int marks){
-        this.name=name;
-        this.roll=roll;
-        this.marks=marks;
-    }
+//     Cons(String name){
+//         this.name=name;
+//     }
+//     Cons(String name,double roll){
+//         this.name=name;
+//         this.roll=roll;
+//     }
+//     Cons(String name,double roll,int marks){
+//         this.name=name;
+//         this.roll=roll;
+//         this.marks=marks;
+//     }
   
 
+// }
+
+// public class Ques{
+// public static void main (String args[]){
+//     Cons obj=new Cons("Ananya");
+//     Cons obj1=new Cons("Ananya",4444);
+//     Cons obj2=new Cons("Ananya",5657,90);
+
+
+// }
+// }
+
+
+
+class Reverse{
+    int arr[];
+    Reverse(int arr[]){
+        this.arr=new int[arr.length];
+        for(int i=0;i<arr.length;i++){
+            this.arr[i]=arr[i];
+        }
+    }
+
+    void rev(){
+        int i=0;
+        int j=arr.length-1;
+        while(i<j){
+            int temp=arr[i];
+            arr [i]=arr[j];
+            arr[j]=temp;
+            i++;
+            j--;
+        }
+
+    }
+    void display(){
+        for(int i:arr){
+            System.out.print(i+" ");
+        }
+    }
 }
 
 public class Ques{
-public static void main (String args[]){
-    Cons obj=new Cons("Ananya");
-    Cons obj1=new Cons("Ananya",4444);
-    Cons obj2=new Cons("Ananya",5657,90);
+    public static void main(String args[]){
+        int arr[]={1,2,3,4,5};
+        Reverse obj=new Reverse(arr);
+        obj.rev();
+        obj.display();
 
+       
 
-}
+    }
 }
