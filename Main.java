03b709bb2c50ec81c9f54a08d3c88723898f8fb8
Ananya Sahu <ananyasahu59513@gmail.com>
2025-04-15 12:45:43 +0530

    Updated code

--- Main.java
+++ Main.java
@@ -1,15 +1,22 @@
 public class Main {
     public static void main(String args[]){
-      int n=12346;
-      int last=n%10;
-      int first=0;
-      while(n!=0){
-        first=n%10;
-        n=n/10;
+        
+      int n=10;
+      if(n<10){
+          System.out.println(n);  
       }
-      System.out.println(first+last);
-    }
-   
+      else{
+        int last=n%10;
+        int first=0;
+        while(n!=0){
+          first=n%10;
+          n=n/10;
+        }
+        System.out.println("The sum of first and last digit is "+ first+last);
+      }
+     
+      }
+     
    
 
     
